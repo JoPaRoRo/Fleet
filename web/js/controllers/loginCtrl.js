1946a982806596928779024fc8adfcce1d795d35
nanoj <nanoj@Pablo-PC>
2016-05-17 12:23:43 -0600

    V7 cambios menores

--- web/js/controllers/loginCtrl.js
+++ web/js/controllers/loginCtrl.js
@@ -4,7 +4,6 @@ angular.module('MetronicApp').controller('LoginCtrl', function ($scope,loginServ
 	$scope.msgtxt='';
         $scope.entrando = false;
 	$scope.login=function(data){
-            alert("entre");
                 $scope.entrando = true;
 		loginService.login(data,$scope); //call login service
 	};
